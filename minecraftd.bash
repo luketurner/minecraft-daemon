72a902d042c677c5ac5f313946cfd56be209e498
Luke Turner <contact@luketurner.org>
2012-06-13 20:06:03 +0100

    Much refactoring. I think this is more idiomatic/safe

--- minecraftd.bash
+++ minecraftd.bash
@@ -10,10 +10,10 @@ SERVER_NAME=`echo $SERVER_LOCATION | sed 's/.*\///'`
 SERVER_DIR=`echo $SERVER_LOCATION | sed "s/\/$SERVER_NAME//"`
 MEMORY_START=32M
 MEMORY_MAX=64M
-BACKUP_DIR=~/home/luke/.minecraft/backups
+BACKUP_DIR=/home/luke/.minecraft/backups
 MAX_BACKUPS=5
 WORLD_NAME=world
-WORLD_DIR=$SERVER_LOCATION/$WORLD_NAME
+WORLD_DIR=$SERVER_DIR/$WORLD_NAME
 
 # Source configuration directory
 # Note configuration file is REQUIRED
@@ -26,62 +26,50 @@ WORLD_DIR=$SERVER_LOCATION/$WORLD_NAME
 #fi
 
 function run_as_user {
-    su -c "$1" $USER
-    echo $1
-    return $?
+    result=`su -c "$1" $USER`
+    echo $result
 }
 
-
 function server_running() {
-    running=`run_as_user "tmux list-sessions | grep $DAEMON"`
-    if [[ running != '' ]]; then
-        return 1
+    result=`run_as_user "tmux list-sessions | grep $DAEMON"`
+    if [[ "$result" != '' ]]; then
+        echo 1
     else
-        return 0
+        echo 0
     fi
 }
 
 function start_server() {
     run_command="cd $SERVER_DIR && java -Xms$MEMORY_START -Xmx$MEMORY_MAX -jar $SERVER_LOCATION nogui"
-    run_as_user "tmux new-session -d -s $DAEMON \"$run_command\""
-    return $?
+    run_as_user "tmux new-session -d -s $DAEMON \"$run_command\"" $stdout>/dev/null
 }
 
 function send_command() {
-    if server_running; then
-        run_as_user "tmux send-keys -t $DAEMON:0.0 \"$1\""
-        run_as_user "tmux send-keys -t $DAEMON:0.0 \"Enter\""
-        return 1
-    else
-        return 0
-    fi
+    run_as_user "tmux send-keys -t $DAEMON:0.0 \"$1\"" $stdout>/dev/null
+    run_as_user "tmux send-keys -t $DAEMON:0.0 \"Enter\"" $stdout>/dev/null
 }
 
 function stop_server() {
     send_command "stop"
-    return $?
 }
 
 case $1 in
     start)
-        echo $SERVER_LOCATION
         stat_busy "Starting $DAEMON"
-        if server_running; then
+        running=`server_running`
+        if [[ $running == 1 ]]; then
             stat_fail
             exit 1
         else
             start_server
-            if [[ "$?" ]]; then
-                stat_done
-            else
-                stat_fail
-                exit 1
-            fi
+            stat_done
         fi
         ;;
     stop)
         stat_busy "Stopping $DAEMON"
-        if stop_server; then
+        running=`server_running`
+        if [[ $running == 1 ]]; then
+            stop_server
             stat_done
         else
             stat_fail
@@ -90,17 +78,30 @@ case $1 in
         ;;
     restart)
         stat_busy "Stopping $DAEMON"
-        if stop_server; then
+        running=`server_running`
+        if [[ $running == 1 ]]; then
+            stop_server
             stat_done
         else
             stat_fail
             exit 1
         fi
-        if start_server; then
+        stat_busy "Starting $DAEMON"
+        running=`server_running`
+        if [[ $running == 1 ]]; then
+            start_server
             stat_done
         else
             stat_fail
             exit 1
+        fi
+        ;;
+    status)
+        running=`server_running`
+        if [[ $running == 1 ]]; then
+            echo "$DAEMON Running"
+        else
+            echo "$DAEMON Stopped"
         fi
         ;;
     backup)
@@ -109,18 +110,22 @@ case $1 in
         if [[ $num_backups > $MAX_BACKUPS ]]; then
             ls -t $BACKUP_DIR | head -n 1 | xargs rm # is xargs really needed?
         fi
-        if [[ ! `send_command "save-all"` ]]; then
-            stat_fail
-            exit 1
+        running=`server_running`
+        if [[ $running == 1 ]]; then
+            send_command "save-all"
+            sleep 3
         fi
-        cp $WORLD_DIR $BACKUP_DIR/`date +%F-%H-%M-%S.backup.d`
+        cp -r $WORLD_DIR $BACKUP_DIR/`date +%F-%H-%M-%S.backup.d`
         stat_done
         ;;
     send)
         stat_busy "Sending command"
-        if send_command $2; then
+        running=`server_running`
+        if [[ $running == 1 ]]; then
+            send_command "$2"
             stat_done
         else
+            echo "Server not running"
             stat_fail
             exit 1
         fi
